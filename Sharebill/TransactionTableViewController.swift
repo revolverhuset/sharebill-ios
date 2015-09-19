fca7e39dd021c0611e8ef3890f689037a56d0e80
Jon Packer <contact@jonpacker.com>
2015-09-19 14:09:09 +0200

    fix typo in name of transaction view controller

---
+++ Sharebill/TransactionTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TranscationTableViewController.swift
+//  Sharebill
+//
+//  Created by Jon Packer on 19.09.15.
+//  Copyright (c) 2015 BRIK. All rights reserved.
+//
+
+import UIKit
+import PromiseKit
+import SwiftyJSON
+
+class TransactionTableViewController: UITableViewController {
+  private var transactions:[JSON]? {
+    didSet {
+      self.tableView.reloadData()
+    }
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    Sharebill.inst.get("recent").then { recents, response -> Void in
+      self.transactions = recents["rows"].arrayValue.map { $0["value"]["meta"] }
+    }
+  }
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return transactions?.count ?? 0
+  }
+
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("transactionCell", forIndexPath: indexPath) as! UITableViewCell
+
+    cell.textLabel?.text = transactions?[indexPath.row]["value"]["meta"]["description"].string ?? "Untitled Transaction"
+
+    return cell
+  }
+  /*
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      // Get the new view controller using [segue destinationViewController].
+      // Pass the selected object to the new view controller.
+  }
+  */
+
+}
